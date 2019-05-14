a1b3104f6fd2416bed779a23d3d4fc3fb418210f
Martin Mach <me@martinmach.cz>
2019-05-14 19:53:24 +0200

    update the example app

--- sandbox/example-app/src/main/java/cz/mxmx/memoryanalyzer/example/App.java
+++ sandbox/example-app/src/main/java/cz/mxmx/memoryanalyzer/example/App.java
@@ -7,42 +7,75 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
-	private List<Integer> sameList = new ArrayList<>();
-	private List<Integer> emptyList = new ArrayList<>(1000);
-	private List<Integer> mostlyEmptyList = new ArrayList<>(1000);
-	private Map<Integer, Object> mostlyEmptyMap = new HashMap<>(1000);
+	private List<Parent> parents;
+	private List<Integer> sameList;
+	private List<Integer> emptyList;
+	private List<Integer> mostlyEmptyList;
+	private List<Integer> listOfDuplicates;
+	private Map<Integer, Object> mostlyEmptyMap;
+	private int childCounter = 0;
+	private int parentCounter = 0;
 
-	public App() throws IOException {
-		Parent parent = new Parent();
-		parent.name = "Parent 1";
+	public App(int run) throws IOException {
+		System.out.format("Running %s iterations...\n", run);
+		this.createLists(run);
+		this.parents = this.createObjectStructure(run);
+		System.out.format("%d objects of each type generated, take the heap dump now and press any key to exit.", run);
+		System.in.read();
+	}
 
-		Child child = new Child();
-		child.name = "Child 1";
-		child.parent = parent;
+	private void createLists(int count) {
+		this.sameList = new ArrayList<>(count * 1000);
+		this.mostlyEmptyMap = new HashMap<>(count * 1000);
+		this.mostlyEmptyList = new ArrayList<>(count * 1000);
+		this.emptyList = new ArrayList<>(count * 1000);
+		this.listOfDuplicates = new ArrayList<>(count * 1000);
 
-		Child child2 = new Child();
-		child2.name = "Child 2";
-		child2.parent = parent;
-
-		Child child3 = new Child();
-		child3.name = "Child 2";
-		child3.parent = parent;
-
-		List<String> list = new ArrayList<String>();
-
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < count; i++) {
 			sameList.add(i);
 		}
 
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < count; i++) {
 			mostlyEmptyList.add(i);
 			mostlyEmptyMap.put(i, "String " + i);
 		}
 
-		System.in.read();
+		for (int i = 0; i < count; i++) {
+			this.listOfDuplicates.add(1);
+		}
+	}
+
+	private List<Parent> createObjectStructure(int count) {
+		List<Parent> parents = new ArrayList<>();
+
+		for (int i = 0; i < count; i++) {
+			Parent parent = this.generateParent(this.parentCounter++);
+
+			for (int k = 0; k < count; k++) {
+				Child child = this.generateChild(i, parent);
+			}
+
+			parents.add(parent);
+		}
+
+		return parents;
+	}
+
+	private Child generateChild(int id, Parent parent) {
+		Child child = new Child();
+		child.name = "Child " + id;
+		child.parent = parent;
+		parent.children.add(child);
+		return child;
+	}
+
+	private Parent generateParent(int id) {
+		Parent parent = new Parent();
+		parent.name = "Parent " + id;
+		return parent;
 	}
 
 	public static void main(String[] args) throws IOException {
-        new App();
+        new App(args.length == 1 ? Integer.parseInt(args[0]) : 1000);
     }
 }
--- sandbox/example-app/src/main/java/cz/mxmx/memoryanalyzer/example/Parent.java
+++ sandbox/example-app/src/main/java/cz/mxmx/memoryanalyzer/example/Parent.java
@@ -1,5 +1,9 @@
 package cz.mxmx.memoryanalyzer.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parent {
     public String name;
+    public List<Child> children = new ArrayList<>();
 }
